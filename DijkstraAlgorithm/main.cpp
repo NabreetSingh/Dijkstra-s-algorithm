b3bb1b01b56c6a6b57490e47017695f746ea3949
Nabreet <nabby.ghumman@gmail.com>
2020-06-04 02:42:20 -0700

    Initial Commit

---
+++ DijkstraAlgorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DijkstraAlgorithm
+//
+//  Created by Nabreet Ghumman on 2020-06-04.
+//  Copyright © 2020 Nabreet Ghumman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
